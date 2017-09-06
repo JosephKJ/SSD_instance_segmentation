3c8473bb447c7ed4f94fda4435fb8204c093b190
Joseph KJ <josephkj20@gmail.com>
2017-09-06 19:09:54 +0530

    Updates

--- examples/ssd/ssd_pascal.py
+++ examples/ssd/ssd_pascal.py
@@ -364,7 +364,7 @@ test_iter = int(math.ceil(float(num_test_image) / test_batch_size))
 
 solver_param = {
     # Train parameters
-    'base_lr': base_lr,
+    'base_lr': 0.0001,
     'weight_decay': 0.0005,
     'lr_policy': "multistep",
     'stepvalue': [80000, 100000, 120000],
